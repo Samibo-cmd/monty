b09056b4e98ee4e5392b3f43390f9325c6a1f3c1
Valentine1244 <59285068+Valentine1244@users.noreply.github.com>
2023-06-22 13:52:53 +0100

    Updated pstr.c

--- pstr.c
+++ pstr.c
@@ -2,7 +2,7 @@
 
 /**
  * f_pstr - prints the entire string starting at the top of the stack
- * @head: pointer to the stack head
+ * @head: points to the stack head
  * @counter: line number
  * Return: nothing
 */
